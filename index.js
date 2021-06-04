8c007a3412e8643f919e983155440ecb3dd4cfd5
Sara Alexie Noomi Nederskov <6583335@rts-365.dk>
2021-06-04 12:08:20 +0200

    added documentation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const formidable = require("express-formidable")
+const cors = require("cors")
 const animals = require("./routes/animals")
 const foods = require("./routes/foods")
 const accessories = require("./routes/accessories")
@@ -11,6 +12,11 @@ const app = express();
 
 // import db-connection
 require("./database")
+
+app.use("/", express.static('docs'))
+
+//allow requests from other origins
+app.use(cors())
 
 // parse http form data
 app.use(formidable())
